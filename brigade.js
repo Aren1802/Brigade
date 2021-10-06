cc60e73842d985464909484e6ab763b864622ba7
Aren Chauhan <89003131+Aren1802@users.noreply.github.com>
2021-10-06 17:19:29 +0530

    Add files via upload

---
+++ brigade.js
@@ -0,0 +1,14 @@
+const { events, Job } = require("brigadier");
+events.on("exec", () => {
+  var dockerBuild = new Job("docker-build")
+  dockerBuild.image = "docker:dind";
+
+  dockerBuild.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 30",
+    "cd /src/",
+    "docker build -t sdobhal369/brigade-test:20 ."
+  ]
+
+  dockerBuild.run();
+});
